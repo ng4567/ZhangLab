82ef6bc846de80c10bde43a94578d9bc1e3811a7
ng4567 <nikhil2@gmail.com>
2017-07-17 15:02:49 -0400

    added ability to read multiple sheets

--- cleandata.py
+++ cleandata.py
@@ -8,21 +8,30 @@ file_name = raw_input("What would you like the filename to be? Make sure to incl
 
 #opens the input file, and then opens the first sheet for reading
 input_file_location = raw_input("What is the location of the input file? Make sure to include .xls at the end!")
-wb = xlrd.open_workbook(input_file_location)
+wb = xlrd.open_workbook(input_file_location, on_demand=True)
 sheet = wb.sheet_by_index(0)
 
+list_of_sheet_names = wb.sheet_names()
+
+
 #copies the values of the columns we are interested in
-file_name_column = sheet.col_values(0)
-response_column = sheet.col_values(4)
-itsd_response_column = sheet.col_values(5)
-sample_id_column = sheet.col_values(17)
+
 
 import xlwt #library needed for writing a new excel file
 workbook = xlwt.Workbook()
-sheet1=workbook.add_sheet("Cleaned Data") #creates a new workbook and then adds a new sheet to it
 
-counter = 0 #for loop that pastes the copies columns into the new file
-for value in range(0,len(file_name_column)):
+#sheet1=workbook.add_sheet("Cleaned Data") #creates a new workbook and then adds a new sheet to it
+
+
+for x in list_of_sheet_names:
+
+   sheet1 = workbook.add_sheet(str(x))
+   file_name_column = sheet.col_values(0)
+   response_column = sheet.col_values(4)
+   itsd_response_column = sheet.col_values(5)
+   sample_id_column = sheet.col_values(17)
+   counter = 0
+   for value in range(0,len(file_name_column)):
 
     sheet1.write(value,0,str(file_name_column[counter]))
     sheet1.write(value,1,str(response_column[counter]))
